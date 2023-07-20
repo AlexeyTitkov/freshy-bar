f73bce0d9cffeb9a71c1073142d81ce074b4ca2a
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-07-20 16:17:49 +0300

    feat: add modal layout

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -21,18 +21,59 @@ const createCard = (item) => {
             <div class="cocktail__content">
               <div class="cocktail__text">
                 <h3 class="cocktail__title">${item.title}</h3>
-                <p class="cocktail__price text-red">${item.price} ₽</p>
+                <p class="cocktail__price text-red">${item.price}&nbsp;₽</p>
                 <p class="cocktail__size">${item.size}</p>
               </div>
 
-              <button class="btn cocktail__btn data-id="${item.id}">Добавить</button>
+              <button class="btn cocktail__btn cocktail__btn_make" data-id="${item.id}">Добавить</button>
             </div>
   `
-  
+
   return cocktail
 }
 
+const modalController = ({modal, btnOpen, time = 300}) => {
+  const buttonElem = document.querySelector(btnOpen)
+  const modalElem = document.querySelector(modal)
+  
+  modalElem.style.cssText = `
+  display: flex;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity ${time}ms ease-in-out;
+  `
+
+  const closeModal = (event) => {
+    const target = event.target
+    const code = event.code
+
+    if (target === modalElem || code === 'Escape') {
+      modalElem.style.opacity = 0;
+      setTimeout(()=>{
+        modalElem.style.visibility = 'hidden';
+      }, time)
+
+      window.removeEventListener('keydown', closeModal)
+    }
+  }
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal)
+  }
+
+  buttonElem.addEventListener('click', openModal)
+  modalElem.addEventListener('click', closeModal)
+
+  return {openModal, closeModal}
+}
+
 const init = async () => {
+  modalController({
+    modal: '.modal_order',
+    btnOpen: '.header__btn-order',
+  })
   const goodsListElem = document.querySelector('.goods__list')
   const data = await getData()
   console.log('data: ', data)
@@ -49,8 +90,5 @@ const init = async () => {
 
 init()
 
-const getDataByIdAndKey = (id, key) => {
-  const item = data.find((element) => element.id === id);
-  return item ? item[key] : null;
-};
+
 
