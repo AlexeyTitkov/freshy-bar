d6baeb76f7d3362e26afac1d5b8192a201f942fc
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-07-24 09:03:46 +0300

    feat: add costing main form

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -67,7 +67,7 @@ const scrollService = {
   }
 }
 
-const modalController = ({modal, btnOpen, time = 300}) => {
+const modalController = ({modal, btnOpen, time = 300, open, close}) => {
   const buttonElems = document.querySelectorAll(btnOpen)
   const modalElem = document.querySelector(modal)
 
@@ -88,13 +88,21 @@ const modalController = ({modal, btnOpen, time = 300}) => {
       setTimeout(() => {
         modalElem.style.visibility = 'hidden';
         scrollService.enabledScroll();
+
+        if (close) {
+          close()
+        }
       }, time)
 
       window.removeEventListener('keydown', closeModal)
     }
   }
 
-  const openModal = () => {
+  const openModal = (e) => {
+    if (open) {
+      open({btn: e.target})
+    }
+
     modalElem.style.visibility = 'visible';
     modalElem.style.opacity = 1;
     window.addEventListener('keydown', closeModal)
@@ -147,9 +155,9 @@ const calculateTotalPrice = (form, startPrice) => {
   } else {
     totalPrice += price[data.toppings] || 0
   }
-  
+
   totalPrice += price[data.cup] || 0
-  
+
   return totalPrice
 }
 
@@ -167,6 +175,48 @@ const calculateMakeYourOwn = () => {
   formMakeYourOwn.addEventListener('change', () => {
     handlerChange()
   })
+}
+
+const calculateAdd = () => {
+  const modalAdd = document.querySelector('.modal_add')
+  const formAdd = document.querySelector('.make__form_add')
+  const makeTitle = modalAdd.querySelector('.make__title')
+  const makeInputTitle = modalAdd.querySelector('.make__input-title')
+  const makeTotalPrice = modalAdd.querySelector('.make__total-price')
+  const makeInputStartPrice = modalAdd.querySelector('.make__input-start-price')
+  const makeInputPrice = modalAdd.querySelector('.make__input-price')
+  const makeTotalSize = modalAdd.querySelector('.make__total-size')
+  const makeInputSize = modalAdd.querySelector('.make__input-size')
+  
+  const handlerChange = () => {
+    const totalPrice = calculateTotalPrice(formAdd, +makeInputStartPrice.value)
+    makeInputPrice.value = totalPrice
+    makeTotalPrice.textContent = `${totalPrice} ₽`
+  }
+  
+  formAdd.addEventListener('change', handlerChange)
+  
+
+  const fillInForm = (data) => {
+    makeTitle.textContent = data.title
+    makeInputTitle.value = data.title
+    makeTotalPrice.textContent = `${data.price} ₽`
+    makeInputStartPrice.value = data.price
+    makeInputPrice.value = data.price
+    makeTotalSize.textContent = data.size
+    makeInputSize.value = data.size
+    handlerChange()
+  }
+
+  const resetForm = () => {
+    makeTitle.textContent = ''
+    makeTotalPrice.textContent = ''
+    makeTotalSize.textContent = ''
+    formAdd.reset()
+  }
+
+
+  return {fillInForm, resetForm}
 }
 
 const init = async () => {
@@ -189,6 +239,8 @@ const init = async () => {
 
   goodsListElem.append(...cartsCocktail)
 
+  const {fillInForm, resetForm} = calculateAdd()
+
   modalController({
     modal: '.modal__main_make-your-own',
     btnOpen: '.cocktail__btn_make',
@@ -197,6 +249,12 @@ const init = async () => {
   modalController({
     modal: '.modal_add',
     btnOpen: '.cocktail__btn_add',
+    open({btn}) {
+      const id = btn.dataset.id
+      const item = data.find(item => item.id.toString() === id)
+      fillInForm(item)
+    },
+    close: resetForm
   })
 
 }
