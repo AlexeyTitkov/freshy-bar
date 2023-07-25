4a2e4e39760ef3d4d558cde263d146010b47644f
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-07-25 15:16:15 +0300

    feat: add decor and animation

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -415,4 +415,32 @@ const init = async () => {
 init()
 
 
+function isTouchDevice() {
+  return 'ontouchstart' in window || navigator.maxTouchPoints;
+}
 
+function onMouseMove(event, moveX, moveY) {
+  if (isTouchDevice()) {
+    return; // Прерываем выполнение кода на сенсорных устройствах
+  }
+
+  event.style.transform = `translate(-${moveX}px, -${moveY}px)`;
+}
+
+document.addEventListener('mousemove', function(e) {
+  const images = document.querySelectorAll('.decor__img');
+  const moves = [
+    { x: 0.4, y: 0.4 },
+    { x: 0.3, y: 0.3 },
+    { x: 0.4, y: 0.4 },
+    { x: 0.7, y: 0.7 },
+    { x: 0.7, y: 0.7 },
+    { x: 0.3, y: 0.3 },
+  ];
+
+  images.forEach((image, index) => {
+    const moveX = (e.clientX * moves[index].x) / 8;
+    const moveY = (e.clientY * moves[index].y) / 8;
+    onMouseMove(image, moveX, moveY);
+  });
+});
